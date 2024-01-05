cf8fed2464c536e94c0b0e84013f6e765a5e45a9
sarmadsalman-RS <sarmad.salman@rapidsilicon.com>
2024-01-05 17:50:48 +0500

    initial support for signed and fractional coefficients

--- rapidsilicon/ip/fir_generator/v1_0/litex_wrapper/fir_litex_generator.py
+++ rapidsilicon/ip/fir_generator/v1_0/litex_wrapper/fir_litex_generator.py
@@ -9,6 +9,7 @@
 import datetime
 import logging
 import math
+import struct
 from migen import *
 import re
 
@@ -33,6 +34,27 @@ def extract_numbers(input_string, coefficients_file):
                 return numbers
         except FileNotFoundError:
             return []
+        
+# Checking if the list of numbers has any negative or fractional number
+def check_negative_or_fractional(number):
+    negative_fractional = number < 0 # or number % 1 != 0
+    return negative_fractional
+
+# Converting fractional or signed number into floating binary and then back to decimal
+def float_to_custom_binary(decimal_number, sign_bits=1, exponent_bits=7, mantissa_bits=12, output_bits=20):
+    # Use struct.pack to convert the decimal number to its binary representation
+    binary_representation = struct.pack('>f', decimal_number)
+
+    # Use binascii.hexlify to convert the binary representation to hexadecimal
+    hexadecimal_representation = bytes.hex(binary_representation)
+
+    # Convert hexadecimal to binary
+    binary_result = bin(int(hexadecimal_representation, 16))[2:]
+
+    # Ensure a custom bit-width representation
+    binary_result = binary_result.zfill(32)[:output_bits]
+
+    return int(binary_result, 2)
 
 # logging.basicConfig(level=logging.INFO)
 logging.basicConfig(filename="IP.log",filemode="w", level=logging.INFO, format='%(levelname)s: %(message)s\n')
@@ -58,17 +80,22 @@ class FIR(Module):
 
         self.logger.info(f"===================================================")
 
-        self.data_in = Signal(input_width)
-        self.data_out = Signal(38)
+        self.data_in = Signal(bits_sign=(input_width, True))
+        self.data_out = Signal(bits_sign=(38, True))
 
         self.z = Array(Signal() for _ in range (len(coefficients)))
         self.delay_b = Array(Signal() for _ in range (len(coefficients)))
 
         for i in range (len(coefficients)):
-            self.delay_b[i] = Signal(input_width, name=f"delay_b_{i}")
-            self.z[i] = Signal(38, name=f"z_{i}")
+            self.delay_b[i] = Signal(bits_sign=(input_width, True), name=f"delay_b_{i}")
+            self.z[i] = Signal(bits_sign=(38, True), name=f"z_{i}")
 
         for i in range(len(coefficients)):
+            if (check_negative_or_fractional(coefficients[i])):
+                a_sign = 0
+            else:
+                a_sign = 1
+            coefficients[i] = float_to_custom_binary(coefficients[i])
             if (i == 0):
                 self.specials += Instance("DSP38",
 
@@ -89,8 +116,8 @@ class FIR(Module):
                     i_B             = self.data_in,
                     o_Z             = self.z[i],  
                     i_FEEDBACK      = C(4, 3),
-                    i_UNSIGNED_A    = 1,
-                    i_UNSIGNED_B    = 1,
+                    i_UNSIGNED_A    = a_sign,
+                    i_UNSIGNED_B    = 0,
                     o_DLY_B         = self.delay_b[i],
                     i_LOAD_ACC      = 1,
                     i_ACC_FIR       = C(0, 6),
@@ -119,8 +146,8 @@ class FIR(Module):
                     i_B             = self.delay_b[i - 1],
                     o_Z             = self.data_out,  
                     i_FEEDBACK      = C(4, 3),
-                    i_UNSIGNED_A    = 1,
-                    i_UNSIGNED_B    = 1,
+                    i_UNSIGNED_A    = a_sign,
+                    i_UNSIGNED_B    = 0,
                     i_LOAD_ACC      = 1,
                     i_ACC_FIR       = C(0, 6),
                     i_ROUND         = 0,
@@ -148,8 +175,8 @@ class FIR(Module):
                     i_B             = self.delay_b[i - 1],
                     o_Z             = self.z[i],  
                     i_FEEDBACK      = C(4, 3),
-                    i_UNSIGNED_A    = 1,
-                    i_UNSIGNED_B    = 1,
+                    i_UNSIGNED_A    = a_sign,
+                    i_UNSIGNED_B    = 0,
                     o_DLY_B         = self.delay_b[i],
                     i_LOAD_ACC      = 1,
                     i_ACC_FIR       = C(0, 6),
